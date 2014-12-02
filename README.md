ffe4b93d73eab27b0150c0a4d9324f6b68c0a803
UMHB Web Services <webservices@umhb.edu>
2014-12-02 13:52:04 -0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,10 @@
+onelogin-event-notification
+==========================
+
+Automatically send event notifications from OneLogin to a specified email address.
+
+This super simple script is intended to fill the gap with OneLogin's notification abilities until they build them in to the product.
+
+Currently, if a directory connector goes down, there is no way to be notified of the event.
+
+We are only running this script once an hour in our environment, but we may change that in the future.
---
+++ event-notify.php
@@ -0,0 +1,52 @@
+<?php    
+
+	// Settings
+	$api_key = 'ENTER-API-KEY-HERE';
+	$to_email = "ENTER@EMAIL.HERE"; //May use comma separated email addresses if more than one recipient
+	$from_email = "ENTER@EMAIL.HERE";
+
+
+    $url = "https://app.onelogin.com/api/v1/events";
+    $ch = curl_init();
+	
+	$hourago=time()-3600;
+	
+	
+    curl_setopt($ch, CURLOPT_USERPWD, $api_key);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, false);
+
+    $http_result = curl_exec($ch);
+    $error       = curl_error($ch);
+    $http_code   = curl_getinfo($ch ,CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+
+    if ($error) {
+      print $error;
+    } else {
+        $http_result=simplexml_load_string($http_result);
+		$i_echoed=0;
+		foreach ($http_result->event as $event) {
+			$notes=(string)$event->notes;
+			$date=strtotime($event->{'created-at'});
+			if (strlen($notes) > 10 && $date >= $hourago) {
+				$output .= $event->{'actor-system'} . " at ". date("g:i a",$date).": " . $notes."\n\n";
+				$i_echoed=1;
+			}
+		}
+		
+		if ($i_echoed==1) {
+			$headers = 'From: ' . $from_email . "\r\n" .
+		    'Reply-To: ' . $from_email . "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
+			
+			$message="The following events have been triggered in OneLogin. Please investigate.\n\n".$output;
+			
+			mail ($to_email,"OneLogin Event Notification",$message,$headers);
+		}
+		
+    }
+
+?>
